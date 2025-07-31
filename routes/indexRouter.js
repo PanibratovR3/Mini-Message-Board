6d4a2f5591d1faa90b502ad1a01fd50f8caa7f69
PanibratovR3 <roman.panibratov@gmail.com>
2025-07-31 16:55:52 +0300

    Created array with messages.

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,5 +1,23 @@
 const { Router } = require("express");
 
+const messages = [
+  {
+    text: "That's, what she said!",
+    user: "Michael",
+    added: new Date(),
+  },
+  {
+    text: "How you doin'?",
+    user: "Joey",
+    added: new Date(),
+  },
+  {
+    text: "I am the Danger!!!",
+    user: "Walter",
+    added: new Date(),
+  },
+];
+
 const indexRouter = Router();
 
 indexRouter.get("/", (request, response) => {
