8ef117e487b0bd0703dcdde3002233a22385e8ee
PanibratovR3 <roman.panibratov@gmail.com>
2025-08-01 10:46:21 +0300

    Added styles for index view.

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const app = express();
 const indexRouter = require("./routes/indexRouter.js");
 
 const PORT = 3000;
+const assetsPath = path.join(__dirname, "public");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(assetsPath));
 
 app.use("/", indexRouter);
 
